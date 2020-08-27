0d5ccc7bff21bd477ff83f601e27a1a89742b752
Moran1324 <67854443+Moran1324@users.noreply.github.com>
2020-08-27 11:32:51 +0300

    supposed to be ready for glitch

--- server/server.js
+++ server/server.js
@@ -4,6 +4,17 @@ const path = require('path');
 
 const app = express();
 
+function checkHttps(request, response, next) {
+    // Check the protocol — if http, redirect to https.
+    if (request.get("X-Forwarded-Proto").indexOf("https") != -1) {
+      return next();
+    } else {
+      response.redirect("https://" + request.hostname + request.url);
+    }
+  }
+  
+  app.all("*", checkHttps)
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../client/build')));
@@ -72,4 +83,25 @@ app.post('/api/tickets/newticket', async (req, res) => {
     res.send("Submitted");
 })
 
-module.exports = app;
+// module.exports = app;
+
+let port;
+console.log("❇️ NODE_ENV is", process.env.NODE_ENV);
+if (process.env.NODE_ENV === "production") {
+  port = process.env.PORT || 3000;
+  app.use(express.static(path.join(__dirname, "../build")));
+  app.get("*", (request, response) => {
+    response.sendFile(path.join(__dirname, "../build", "index.html"));
+  });
+} else {
+  port = 3001;
+  console.log("⚠️ Not seeing your changes as you develop?");
+  console.log(
+    "⚠️ Do you need to set 'start': 'npm run development' in package.json?"
+  );
+}
+
+// Start the listener!
+const listener = app.listen(port, () => {
+  console.log("❇️ Express server is running on port", listener.address().port);
+});
